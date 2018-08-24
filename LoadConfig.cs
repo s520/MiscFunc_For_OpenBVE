31f5962780da5a6a3a9d5d3f7e6ad8bd93751bfd
S520 <29241703+s520@users.noreply.github.com>
2018-08-24 20:07:37 +0900

    Initialize import

---
+++ LoadConfig.cs
@@ -0,0 +1,75 @@
+﻿using IniParser;
+using IniParser.Model;
+using System.IO;
+
+namespace MiscFunc {
+
+    /// <summary>
+    /// 設定を読み込むクラス
+    /// </summary>
+    internal class LoadConfig {
+
+        // --- メンバ ---
+        private static LoadConfig load_config_ = new LoadConfig();
+
+        internal static int WiperRate { get; private set; }
+        internal static int WiperHoldPosition { get; private set; }
+        internal static int WiperDelay { get; private set; }
+        internal static int WiperSoundBehaviour { get; private set; }
+        internal static int WiperWet { get; private set; }
+
+
+        // --- コンストラクタ ---
+        /// <summary>
+        /// 新しいインスタンスを作成する
+        /// </summary>
+        private LoadConfig() {
+            WiperRate = 700;
+            WiperHoldPosition = 0;
+            WiperDelay = 700;
+            WiperSoundBehaviour = 0;
+            WiperWet = 0;
+        }
+
+        // --- 関数 ---
+        /// <summary>
+        /// インスタンスを取得するメソッド
+        /// </summary>
+        /// <returns>インスタンス</returns>
+        internal static LoadConfig GetInstance() {
+            return load_config_;
+        }
+
+        /// <summary>
+        /// ファイルから設定を読み込む関数
+        /// </summary>
+        /// <param name="filePath">ファイルパス</param>
+        internal void LoadCfgFile(string filePath) {
+            if (File.Exists(filePath)) {
+                //Create an instance of a ini file parser
+                FileIniDataParser fileIniData = new FileIniDataParser();
+
+                //Parse the ini file
+                IniData parsedData = fileIniData.ReadFile(filePath);
+
+                //Get concrete data from the ini file
+                int value;
+                if (int.TryParse(parsedData["Wiper"]["WiperRate"], out value)) {
+                    WiperRate = value;
+                }
+                if (int.TryParse(parsedData["Wiper"]["WiperHoldPosition"], out value)) {
+                    WiperHoldPosition = value;
+                }
+                if (int.TryParse(parsedData["Wiper"]["WiperDelay"], out value)) {
+                    WiperDelay = value;
+                }
+                if (int.TryParse(parsedData["Wiper"]["WiperSoundBehaviour"], out value)) {
+                    WiperSoundBehaviour = value;
+                }
+                if (int.TryParse(parsedData["Wiper"]["WiperWet"], out value)) {
+                    WiperWet = value;
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,37 @@
+﻿# MiscFunc For OpenBVE
+
+Misc function Plugin for OpenBVE on Linux or Windows
+
+このプラグインは.NETアセンブリプラグインです。
+
+## Description
+
+様々な機能を再現するOpenBVE用プラグインです。
+
+詳細な仕様は[ATCFS Specifications.pdf](https://github.com/s520/ATCFS_For_OpenBVE/blob/master/doc/ATCFS%20Specifications.pdf)をご覧ください。
+
+ドキュメントは[こちら]()。
+
+## Features
+
+- ワイパーの動作を再現
+- 電流計を再現
+- [DetailManager For OpenBVE](https://github.com/s520/DetailManager_For_OpenBVE)を用いて[ATCFS For OpenBVE](https://github.com/s520/ATCFS_For_OpenBVE)と組み合わせることが可能
+
+## Requirement
+
+- Visual Studio 2010 or more
+
+## License
+
+2-clause BSD license
+
+- Since this project contains part BVEC_ATS source code, that part is provided under the 2-clause BSD license of BVEC_ATS described below.
+- Since this project contains INI File Parser source code, that part is provided under the MIT license of INI File Parser described below.
+- Since this project contains CSV Reader source code, that part is provided under the MIT license of CSV Reader described below.
+
+## Postscript
+
+本プロジェクトの英語への翻訳にご協力いただける方を募集しております。
+
+翻訳にご協力いただける方はpull requestを送ってください。
